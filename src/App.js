52f4c41397a543fae0a859c5bf96e34c1395157b
arihant557 <5arihant.jain5@gmail.com>
2020-09-10 02:32:31 +0530

    adding text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [input, setInput] = useState('');
+
   return (
     <div className="App">
 
 <h1> Facebook messgenger clone </h1>
+
+<input value = {input} onChange = {event => setInput(event.target.value)} />
+<button>Send</button>
     
      </div>
   );
