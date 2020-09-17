5540d57979d648ab471065da6a64b4221b084f7f
arihant557 <5arihant.jain5@gmail.com>
2020-09-17 23:36:43 +0530

    usernames working

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Message from './Message';
 function App() {
 
 const [input, setInput] = useState('');
-const [messages , setMessages] = useState([]);
+const [messages , setMessages] = useState([{username:'Arihant' , text:'Hey how r you'}, {username: 'Rihan' , text:'GOOD , You tell'}]);
 const [username , setUsername] = useState('');
 
 /*console.log(input);
@@ -16,7 +16,7 @@ console.log(messages);
 
 useEffect(() => {
 
-  //const username = prompt("Please enter your name")
+  //const username = prompt("Please enter your name");
   setUsername(prompt('Please enter your name'));
 
 }, [input])
@@ -24,7 +24,7 @@ useEffect(() => {
 const sendMessage = (event) =>{
 
    event.preventDefault();
-   setMessages([...messages, input]);
+   setMessages([...messages, {username: username, text: input}]);
    setInput('');
 }
 
@@ -45,7 +45,7 @@ const sendMessage = (event) =>{
 </form>
 {
   messages.map(message => (
-    <Message text={message} />
+    <Message username={message.username} text={message.text} />
   ))
 }
     
