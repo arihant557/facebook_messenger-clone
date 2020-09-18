339180fd6b9cc0f24e5f587793f49c77cf9ee2ae
arihant557 <5arihant.jain5@gmail.com>
2020-09-18 16:28:50 +0530

    concating by objects

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Message from './Message';
 function App() {
 
 const [input, setInput] = useState('');
-const [messages , setMessages] = useState([{username:'Arihant' , text:'Hey how r you'}, {username: 'Rihan' , text:'GOOD , You tell'}]);
+const [messages , setMessages] = useState([{username:'Facebook', text:'Welcome to messenger' }]);
 const [username , setUsername] = useState('');
 
 /*console.log(input);
@@ -17,9 +17,9 @@ console.log(messages);
 useEffect(() => {
 
   //const username = prompt("Please enter your name");
-  setUsername(prompt('Please enter your name'));
+  setUsername(prompt('Please enter your name' ));
 
-}, [input])
+}, []);
 
 const sendMessage = (event) =>{
 
@@ -31,7 +31,7 @@ const sendMessage = (event) =>{
   return ( 
     <div className="App">
 
-<h1> Facebook messgenger clone </h1>
+<h1> <i>  Messenger </i>  </h1>
 <h2> Welcome {username} </h2>
 
 <form>
