a74dfff710f26802b34343679cd5b57311cc9ef4
arihant557 <5arihant.jain5@gmail.com>
2020-09-18 23:29:39 +0530

    styling

--- src/Message.js
+++ src/Message.js
@@ -1,10 +1,23 @@
+//import { CardContent, Typography } from '@material-ui/core';
 //es7 
-import React from 'react'
+import React from 'react'; 
+import { Card , CardContent , Typography} from '@material-ui/core';
 
 export default function Message(props) {
     return (
-        <div>
-            <h2>{props.username}: {props.text}</h2>
-        </div>
+
+            <Card>
+                <CardContent>
+                    <Typography
+                    color = "white"
+                    variant = "h5"
+                    component = "h2"
+                    >
+                        <h2>{props.username}: {props.text}</h2>
+
+                    </Typography>
+                </CardContent>
+            </Card>
+            
     )
 }
