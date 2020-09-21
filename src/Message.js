28019acf709b65c2f14e8191e71a9ea4ee5ea9bd
arihant557 <5arihant.jain5@gmail.com>
2020-09-22 01:45:53 +0530

    alligning usernames

--- src/Message.js
+++ src/Message.js
@@ -5,8 +5,10 @@ import { Card , CardContent , Typography} from '@material-ui/core';
 import './Message.css';
 
 export default function Message({message,username}) {
+    const isUser = username === message.username;
     return (
-             <Card className = {'message'}>
+             <div className ={`message ${isUser && 'message__user'}`}>
+                 <Card className ={isUser ? "message__userCard" : "message__guestCard"}>
                 <CardContent>
                 <Typography
                     color = "white"
@@ -17,5 +19,6 @@ export default function Message({message,username}) {
                  </Typography>
                 </CardContent>
              </Card>       
+             </div>
     )
 }
